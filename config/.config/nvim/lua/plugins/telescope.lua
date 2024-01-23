2f59b63aa9e8ef95f7031cc60a5d7fd58ea4556e
jchen13 <jchen13@humana.com>
2024-01-23 15:34:26 -0800

    Add config for venv selector and update telescope

--- config/.config/nvim/lua/plugins/telescope.lua
+++ config/.config/nvim/lua/plugins/telescope.lua
@@ -1,13 +1,20 @@
 -- change some telescope options and a keymap to browse plugin files
 return {
-  "nvim-telescope/telescope.nvim",
-  -- change some options
-  opts = {
-    defaults = {
-      layout_strategy = "horizontal",
-      layout_config = { prompt_position = "top" },
-      sorting_strategy = "ascending",
-      winblend = 0,
-    },
-  },
+	"nvim-telescope/telescope.nvim",
+	-- change some options
+	opts = {
+		defaults = {
+			layout_strategy = "horizontal",
+			layout_config = { prompt_position = "top" },
+			sorting_strategy = "ascending",
+			winblend = 0,
+		},
+	},
+	dependencies = {
+		"nvim-telescope/telescope-fzf-native.nvim",
+		build = "make",
+		config = function()
+			require("telescope").load_extension("fzf")
+		end,
+	},
 }
